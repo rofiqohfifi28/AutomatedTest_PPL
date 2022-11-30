ffe85131a9846059ac99315a54bd6edac85412df
rofiqohfifi28 <99854026+rofiqohfifi28@users.noreply.github.com>
2022-11-30 09:15:57 +0700

    Add files via upload

---
+++ Registrasi.cy.js
@@ -0,0 +1,84 @@
+describe('empty spec', () => {
+  it('passes', () => {
+    cy.visit('http://localhost/pet_shop/')
+  //  cy.get('#login-btn').click()
+   // cy.get('#create_account').trigger('click')
+
+
+    /*cy.get('.col-lg-5 > :nth-child(1) > .form-control').type('rofiqoh')
+    cy.get('.col-lg-5 > :nth-child(2) > .form-control').type('wahyuningtyas')
+    cy.get('.col-lg-5 > :nth-child(3) > .form-control').type('08976543212')
+    cy.get('.custom-select').select('Female')
+    cy.get('.col-lg-7 > :nth-child(1) > .form-control').type('mondorko')
+    cy.get('.col-lg-7 > :nth-child(2) > .form-control').type('rofifiq28@gmail.com')
+    cy.get('.col-lg-7 > :nth-child(3) > .form-control').type('fifi1234')
+    cy.get('.col-lg-7 > .d-flex > .btn').click()
+    */
+    
+
+   /*cy.get('.col-lg-5 > :nth-child(1) > .form-control').type('rofiqoh')
+    cy.get('.col-lg-5 > :nth-child(2) > .form-control')
+    cy.get('.col-lg-5 > :nth-child(3) > .form-control').type('08976543212')
+    cy.get('.custom-select').select('Female')
+    cy.get('.col-lg-7 > :nth-child(1) > .form-control').type('mondorko')
+    cy.get('.col-lg-7 > :nth-child(2) > .form-control').type('rofiqqqoh@gmail.com')
+    cy.get('.col-lg-7 > :nth-child(3) > .form-control').type('fifi1234')
+    cy.get('.col-lg-7 > .d-flex > .btn').click()
+    */
+
+    /*cy.get('.col-lg-5 > :nth-child(1) > .form-control').type('rofiqoh')
+    cy.get('.col-lg-5 > :nth-child(2) > .form-control').type('wahyuningtyas')
+    cy.get('.col-lg-5 > :nth-child(3) > .form-control')
+    cy.get('.custom-select').select('Female')
+    cy.get('.col-lg-7 > :nth-child(1) > .form-control').type('mondorko')
+    cy.get('.col-lg-7 > :nth-child(2) > .form-control').type('rofiqohh11@gmail.com')
+    cy.get('.col-lg-7 > :nth-child(3) > .form-control').type('fifi1234')
+    cy.get('.col-lg-7 > .d-flex > .btn').click()
+    */
+
+    /*cy.get('.col-lg-5 > :nth-child(1) > .form-control').type('rofiqoh')
+    cy.get('.col-lg-5 > :nth-child(2) > .form-control').type('wahyuningtyas')
+    cy.get('.col-lg-5 > :nth-child(3) > .form-control').type('08976543212')
+    cy.get('.custom-select').select('Female')
+    cy.get('.col-lg-7 > :nth-child(1) > .form-control')
+    cy.get('.col-lg-7 > :nth-child(2) > .form-control').type('rofiqohyusu54@gmail.com')
+    cy.get('.col-lg-7 > :nth-child(3) > .form-control').type('fifi1234')
+    cy.get('.col-lg-7 > .d-flex > .btn').click()
+    */
+
+    /*cy.get('.col-lg-5 > :nth-child(1) > .form-control').type('rofiqoh')
+    cy.get('.col-lg-5 > :nth-child(2) > .form-control').type('wahyuningtyas')
+    cy.get('.col-lg-5 > :nth-child(3) > .form-control').type('08976543212')
+    cy.get('.custom-select').select('Female')
+    cy.get('.col-lg-7 > :nth-child(1) > .form-control').type('mondorko')
+    cy.get('.col-lg-7 > :nth-child(2) > .form-control')
+    cy.get('.col-lg-7 > :nth-child(3) > .form-control').type('fifi1234')
+    cy.get('.col-lg-7 > .d-flex > .btn').click()
+    */
+    
+
+    /*cy.get('.col-lg-5 > :nth-child(1) > .form-control').type('rofiqoh')
+    cy.get('.col-lg-5 > :nth-child(2) > .form-control').type('wahyuningtyas')
+    cy.get('.col-lg-5 > :nth-child(3) > .form-control').type('08976543212')
+    cy.get('.custom-select').select('Female')
+    cy.get('.col-lg-7 > :nth-child(1) > .form-control').type('mondorko')
+    cy.get('.col-lg-7 > :nth-child(2) > .form-control').type('rofiiqohyu897@gmail.com')
+    cy.get('.col-lg-7 > :nth-child(3) > .form-control')
+    cy.get('.col-lg-7 > .d-flex > .btn').click()
+    */
+       cy.get('#login-btn').click()
+        cy.get('#login-form > :nth-child(1) > .form-control').type('fifi@gmail.com')
+        cy.get(':nth-child(2) > .form-control').type('fifi1111')
+        cy.get('#login-form > .d-flex > .btn').click()
+        cy.get(':nth-child(1) > .card > .card-footer > .text-center > .btn').click()
+        cy.get('#add-cart > .d-flex > .btn').click()
+        cy.get('#login-form > :nth-child(1) > .form-control').type('fifi@gmail.com')
+        cy.get(':nth-child(2) > .form-control').type('123')
+        cy.get('#login-form > .d-flex > .btn').click()
+    
+       
+    
+      
+      })
+    })
+    
